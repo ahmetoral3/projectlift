0311ff034d8e0dace848b1d8e5c393fb251ffb99
MarcusTalbotS <90057025+MarcusTalbotS@users.noreply.github.com>
2021-09-23 13:57:18 +0200

    Created keypad.ino
    Created the backbone for the keypad. Will be hooked up to the Arduino Mega, so this code will be integrated in there.

---
+++ keypad/keypad/keypad.ino
@@ -0,0 +1,50 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Four columns
+
+// TODO set correct pin-layout
+//// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to row pinouts
+
+// TODO set correct pin-layout
+// Connect keypad COL0, COL1, COL2 and COL3 to these Arduino pins.
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to column pinouts
+
+//Define the keymap
+char keys[ROWS][COLS] = {
+{'1','2','3','A'},
+{'4','5','6','B'},
+{'7','8','9','C'},
+{'*','0','#','D'}
+};
+
+Keypad keypad - Keypad( makeKeymap(keys), rowPins, colPins);
+
+void setup() {
+  // No real setup for keypad
+  Serial.begin(9600);
+}
+
+void loop() {
+  char key = keypad.getKey();
+  if (key != NO_KEY) {
+    delay(60);
+    switch (key) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+        processKey(key);
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+void processKey(char key) {
+  Serial.print(key); // Print key for debugging
+  // Send message to Machine room, to process the key.
+}
