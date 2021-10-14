7fd81a14f18995fcaf68cf47e0d53699daa0cf1c
Marcus Talbot <90057025+MarcusTalbotS@users.noreply.github.com>
2021-10-14 14:17:51 +0200

    Update Master_Combined.ino

--- Master_Combined.ino
+++ Master_Combined.ino
@@ -41,13 +41,9 @@ char keys[ROWS][COLS] = {
  * Dan shiften we één bit naar links:
  * etage = etage << 1;
  * 
- * Nu kunnen we de meest linkse bit (LSB) 0 laten voor naar beneden, of 1 maken voor 
+ * Nu kunnen we de meest rechter bit (LSB) 0 laten voor naar beneden, of 1 maken voor 
  * naar boven.
  * 
- * Hoewel de array gevuld is met 0, kunnen we toch 0 gebruiken voor: Naar beneden, naar 
- * de begane grond.
- * 
- * Dit doen we door bij te houden hoeveel elementen er in de destinations-array zitten.
  */
 byte destinations[6];
 byte destinationsLeft = 0;
@@ -56,6 +52,7 @@ byte currentFloor = 0;
 byte destinationFloor = 0;
 byte currentDirection = STATIONARY;
 
+// Initieer de keypad.
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
@@ -70,17 +67,18 @@ void loop() {
 
 }
 
+/**
+ * Leest data via I2C, en roept de juiste handle-functies aan.
+ */
 void receiveEvent(int numOfBytes) {
   // TODO handle data...
   while (Wire.available()) {
     byte data = Wire.read();
-    bool functionIdentifier = data % 2;
-    if (functionIdentifier) {
-      // IR-signal
-      handleIRSignal(data >> 1);
-    } else {
-      // Button pressed
-      handleButtonPress(data >> 1);
+    bool functionIdentifier = data % 2; // Check of LSB 1 of 0 is.
+    if (functionIdentifier) { // Als LSB 1 is, dan is de data IR-data.
+      handleIRSignal(data >> 1); // Shift naar rechts om de functionIdentifier te verwijderen, en roep de handle-functie aan.
+    } else { // Als LSB 0 is, dan is de data een button-press. 
+      handleButtonPress(data >> 1); // Shift naar rechts om de functionIdentifier te verwijderen, en roep de handle-functie aan.
     }
   }
 }
@@ -89,25 +87,32 @@ void receiveEvent(int numOfBytes) {
  * Inserts a destination at the given index of the destinations-array.
  */
 void insertInDestinationArray(int index, byte destination) {
-  if (destinationsLeft < index) {
+  // Als de index groter is dan destinationsLeft, dan valt de index buiten de functionele array.
+  if (index > destinationsLeft) { 
     cout << "Index is greater than amount of destinations in array." << endl;
+    return;
   }
-  
+
+  // We maken een nieuwe array, en kopiëren daar alle waarden voor de gegeven index naartoe.
   byte newDestinations[6];
   for (auto i = 0; i < index; i++) {
     newDestinations[i] = destinations[i];
   }
 
+  // De gegeven index is bereikt, dus zetten we de waarde erin.
   newDestinations[index] = destination;
 
+  // We kopiëren de rest van de destinations in de destinations-array naar de kopie-array.
   for (auto i = 0; i < destinationsLeft - index; i++) {
     newDestinations[i + index] = destinations[i + index];
   }
 
+  // We kopiëren de waardes in de kopie-array naar de destinations-array.
   for (auto i = 0; i < 6; i++) {
     destinations[i] = newDestinations[i];
   }
 
+  // Incrementeer de hoeveelheid destinations in de array.
   destinationsLeft += 1;
 }
 
@@ -115,11 +120,15 @@ void insertInDestinationArray(int index, byte destination) {
  * Removes the entry at the given index of the destinations-array.
  */
 void removeAtIndex(int index) {
+  // Als de index kleiner is dan destinationsLeft, dan is het een valide index.
   if (index < destinationsLeft) {
+    // Vanaf de index die verwijderd moet worden, schuiven we alle waardes in de destinationsArray een plaats op naar links.
     for (auto i = index; index < 5; index++) {
       destinations[i] = destinations[i + 1];
     }
+    destinations[5] = 0;
   }
+  // Decrementeer de hoeveelheid
   destinationsLeft -= 1;
 }
 
@@ -140,8 +149,12 @@ void handleButtonPress(byte data) {
 
 void readKeypad() {
   char key = keypad.getKey();
-  if (key != NO_KEY) {
-    delay(60);
+  if (key != NO_KEY) { // Dit voeren we alleen uit als er WEL een knop ingedrukt is.
+    /*
+     * Wanneer een case waar is, dan zal de switch alles daaronder uitvoeren, tot het een break-statement tegenkomt. Dit betekent dat we voor de waardes 
+     * 0 t/m 3 maar 1 keer processKey hoeven neer te zetten, gevolgd door een break. Alle waardes daarboven zijn niet relevant voor onze lift, dus 
+     * die negeren we.
+     */
     switch (key) {
       case '0':
       case '1':
@@ -156,5 +169,33 @@ void readKeypad() {
 }
 
 void processKey(char key) {
-  // Add a destination to the master's destinations.
+  
+  byte data;
+  
+  switch (key) {
+    case '0':
+      data = 0;
+      break;
+    case '1':
+      data = 1;
+      break;
+    case '2':
+      data = 2;
+      break;
+    case '3':
+      data = 3;
+      break;
+    default:
+      break;
+  }
+
+  if (data == currentFloor) {
+    return;
+  }
+
+  data = data << 1;
+  data += (data > currentFloor) ? 1 : 0;
+  
+  handleButtonPress(data);
+  
 }
